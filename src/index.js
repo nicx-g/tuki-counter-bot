cef19cec954548ae179d2f57312013c05102ad3d
Nicolás Gomez <nicx_g@hotmail.com>
2021-06-10 23:27:45 -0300

    Reorganicé las carpetas, ahora también cuenta los 'nashe' que escribas y se adaptó a las stats

--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ client.on('message', async (message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift();
 
+    //TUKI COUNTER
     if(message.content.toLowerCase().includes("tuki")){
         User.findOne({userId: message.author.id}, (error, user) => {
             if(error)console.log(error)
@@ -59,6 +60,13 @@ client.on('message', async (message) => {
                             serverId: message.guild.id,
                             tukiCounter: 1
                         }
+                    ],
+                    nashesPerServer:[
+                        {
+                            serverName: message.guild.name,
+                            serverId: message.guild.id,
+                            nasheCounter: 0
+                        }
                     ]
                 })
                 newUser.save()
@@ -83,19 +91,87 @@ client.on('message', async (message) => {
             })
         }, 500)
     }
+    //NASHE COUNTER
+    if(message.content.toLowerCase().includes("nashe")){
+        User.findOne({userId: message.author.id}, (error, user) => {
+            if(error)console.log(error)
+            if(user){
+                let serverId = user.nashesPerServer.filter(item => item.serverId === message.guild.id)
+                if(serverId.length === 0){
+                    user.nashesPerServer.push({
+                        serverName: message.guild.name,
+                        serverId: message.guild.id,
+                        nasheCounter: 1
+                    })
+                    user.save().catch(err => console.log(err))
+                } else {
+                    let indexServer = user.nashesPerServer.findIndex(item => item.serverId === message.guild.id)
+                    let newNasheCounter = user.nashesPerServer[indexServer].nasheCounter + 1
+                    User.updateOne({"nashesPerServer.serverId":message.guild.id, userId: message.author.id}, {"nashesPerServer.$.nasheCounter": newNasheCounter}, (error, rep) => {
+                        if(error)console.log(error);
+                    })
+                }
+            } else {
+                const newUser = new User({
+                    username: message.author.username,
+                    userId: message.author.id,
+                    avatar: message.author.avatarURL(),
+                    discriminator: message.author.discriminator,
+                    tukisPerServer:[
+                        {
+                            serverName: message.guild.name,
+                            serverId: message.guild.id,
+                            tukiCounter: 0
+                        }
+                    ],
+                    nashesPerServer:[
+                        {
+                            serverName: message.guild.name,
+                            serverId: message.guild.id,
+                            nasheCounter: 1
+                        }
+                    ]
+                })
+                newUser.save()
+            }
+        })
+        setTimeout(async () => {
+            await User.find({}).then(rep => {
+                let totalServerNashe = rep.reduce((acc,valor) => {
+                    let nashesArray = []
+                    valor.nashesPerServer.map(item => {
+                        if(item.serverId === message.guild.id) {
+                            nashesArray.push(item.nasheCounter)
+                        }
+                    })
+                    let nashes = nashesArray.length !== 0 ? nashesArray.reduce((acc, valor) => {return acc + valor}) : 0
+                    return acc + nashes
+                }, 0)
+                let embed = new MessageEmbed()
+                .setColor('ffa07a')
+                .setDescription(`opa, vamos \`${totalServerNashe}\` nashes en el server pa`)
+                message.channel.send(embed)                
+            })
+        }, 500)
+    }
     if(command == 'stats' && message.content.startsWith(prefix)){
         let userIdTarget = args.join('').slice(3, args.join('').length - 1)
         if(userIdTarget){
-            await User.find({userId: userIdTarget})
+            await User.findOne({userId: userIdTarget})
             .then(rep => {
                 let tukiQtyInThisServer = []
-                rep[0].tukisPerServer.map(item => {
+                rep.tukisPerServer.map(item => {
                     if(item.serverId === message.guild.id) tukiQtyInThisServer.push(item.tukiCounter)
                 })
+                let nasheQtyInThisServer = []
+                rep.nashesPerServer.map(item => {
+                    if(item.serverId === message.guild.id) nasheQtyInThisServer.push(item.nasheCounter)
+                })
+                let nasheQty = nasheQtyInThisServer.length === 0 ? 0 : nasheQtyInThisServer
                 let embed = new MessageEmbed()
                 .setColor('ffa07a')
-                .setAuthor(rep[0].username, rep[0].avatar)
-                .setDescription(`Tiene \`${tukiQtyInThisServer[0]}\` tukis en el server`)
+                .setAuthor(rep.username, rep.avatar)
+                .setDescription(`Tiene \`${tukiQtyInThisServer[0]}\` tukis y \`${nasheQty}\` nashes en el server`)
                 message.channel.send(embed)
             })
             .catch(error => {
@@ -140,21 +216,66 @@ client.on('message', async (message) => {
                 let sortingArrayAscendent = tukisPerUser.sort((a,b) => {return b.tukiCounter - a.tukiCounter})
                 let top5 = []
                 sortingArrayAscendent.map((item, index) => {
-                    if(index >= 4)top5.push(item);
+                    if(index <= 4)top5.push(item);
                 })
-                return tukisPerUser;
+                return top5;
             })
-            let top5 = [];
+            let nashesInThisServer = await User.find({}).then(rep => {
+                let totalServerNashe = rep.reduce((acc,valor) => {
+                    let nasheArray = []
+                    valor.nashesPerServer.map(item => {
+                        if(item.serverId === message.guild.id) {
+                            nasheArray.push(item.nasheCounter)
+                        }
+                    })
+                    let nashes = nasheArray.length !== 0 ? nasheArray.reduce((acc, valor) => {return acc + valor}) : 0
+                    return acc + nashes
+                }, 0)
+                return totalServerNashe
+            })
+            let top5NashePerUser = await User.find({}).then(rep => {
+                let nashesPerUser = []
+                rep.map(item => {
+                    let userInfo = {
+                        userId: item.userId,
+                        username: item.username,
+                        discriminator: item.discriminator,
+                        avatar: item.avatar
+                    };
+                    item.nashesPerServer.map(nashes => {
+                        if(nashes.serverId === message.guild.id){
+                            nashesPerUser.push({
+                                ...userInfo,
+                                nasheCounter: nashes.nasheCounter
+                            })
+                        }
+                    })
+                })
+                let sortingArrayAscendent = nashesPerUser.sort((a,b) => {return b.nasheCounter - a.nasheCounter})
+                let top5 = []
+                sortingArrayAscendent.map((item, index) => {
+                    if(index <= 4)top5.push(item);
+                })
+                return top5;
+            })
+            let top5Tukis = [];
             top5TukisPerUser.map(item => {
-                top5.push(`${item.username}: \`${item.tukiCounter}\`\n`)
+                top5Tukis.push(`${item.username}: \`${item.tukiCounter}\`\n`)
             })
-            let top5string = top5.join('')
+            let top5Nashes = [];
+            top5NashePerUser.map(item => {
+                top5Nashes.push(`${item.username}: \`${item.nasheCounter}\`\n`)
+            })
+            let top5TukiString = top5Tukis.join('')
+            let top5NasheString = top5Nashes.join('')
             let embed = new MessageEmbed()
             .setColor('ffa07a')
             .setThumbnail(top5TukisPerUser[0].avatar)
-            .setTitle(`Tenemos \`${tukisInThisServer}\` tukis en el server paaa`)
-            .setDescription(`Top 5:\n
-            ${top5string}
+            .setTitle(`Tenemos \`${tukisInThisServer}\` tukis y \`${nashesInThisServer}\` nashes en el server paaa`)
+            .setDescription(`Top 5 Tukis:\n
+            ${top5TukiString}
+            Top 5 Nashe:\n
+            ${top5NasheString}
             Si querés saber los tukis de alguien en particular usá \`${prefix}stats @usuario\``)
             message.channel.send(embed)
         }
@@ -167,7 +288,7 @@ client.on('message', async (message) => {
             },
             {
                 name: `${prefix}stats`,
-                value: `Para saber las estadísticas del servidor, este te mostrará la cantidad de Tukis actuales y el top 5 de los vírgenes que más tukis pusieron, si querés saber cuántos tukis puso en específico, lo podés taggear. Ejemplo: \`${prefix}stats @usuario\``
+                value: `Para saber las estadísticas del servidor, este te mostrará la cantidad de Tukis actuales y el top 5 de los vírgenes que más tukis y nashes pusieron, si querés saber cuántos tukis y nashes puso en específico, lo podés taggear. Ejemplo: \`${prefix}stats @usuario\``
             }
         ]
         let embed = new MessageEmbed()
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     userId: Number,
     tukisPerServer: Array,
+    nashesPerServer: Array,
     username: String,
     discriminator: Number,
     avatar: String
